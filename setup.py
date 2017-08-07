464124ab71032c8cabfc12bec6fdf4ebcf5b0103
JeongJu Kim <haje01@gmail.com>
2017-08-07 16:41:20 +0900

    dependency for pandas

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import sys
 from setuptools import setup, find_packages
 
-if sys.version_info < (3,5):
+if sys.version_info < (3, 5):
     sys.exit('Sorry, Python < 3.5 is not supported!')
 
 setup(name='gym_tictactoe',
       version='0.0.1',
-      install_requires=['gym', 'click', 'tqdm'],
+      install_requires=['gym', 'click', 'tqdm', 'pandas'],
       packages=find_packages()
       )
